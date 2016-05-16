76b926c2634b24f2339b2991ae607fb30746c436
JIAO Jie <thomasjiao@vip.qq.com>
2016-05-16 15:25:59 +0800

    redbeans orm test

--- routers/index.router.php
+++ routers/index.router.php
@@ -8,6 +8,12 @@ $app->get('/', function () use ($app) {
 });
 
 $app->get('/hello/:user', function ($user) use ($app) {
+//    $app->render('index.html', array('hello' => "hello, $user"));
+    $app->etag("{$user}_" . date("Ymd"), "weak");
+    $app->flash("user", [rand()]);
+    \RedBeanPHP\R::setup('mysql:host=172.16.7.27:3306;dbname=duizhang', 'jiaojie', '<Stp123>');
+    $temp = \RedBeanPHP\R::find("pay_order", "sid = ?", [3822007107]);
+    \Kint::dump($temp);
     $app->render('index.html', array('hello' => "hello, $user"));
 })->name("test");
 
@@ -16,11 +22,13 @@ $app->get('/hello/:user', function ($user) use ($app) {
 $getRoute = function(\Slim\Route $route) use($app) {
     \Kint::dump($route);
     \Kint::dump($app->urlFor("test", ["user" => "world"]));
+    \Kint::dump($_SERVER);
 };
 
 $app->get('/hello/:user/:word', $getRoute, function ($user, $word) use ($app) {
 //    $app->halt(403, 'You shall not pass!');
-    $a =b;
+//    $app->expires('+1 week');
+    $app->etag("1-{$user}-{$word}", "weak");
     $app->render('index.html', array('hello' => "hay, $word, $user"));
 });
 
